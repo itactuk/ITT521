e295faab30bdf1e2cd06f7f924bd2c08ce1d872a
Ivan <ivantm24@gmail.com>
2020-02-12 19:11:40 -0400

    actualiza codigo para facilitar captura

--- clase4_disectando_paquetes/cliente_netpro.py
+++ clase4_disectando_paquetes/cliente_netpro.py
@@ -3,40 +3,52 @@ import socket
 # version (1 byte), tipo (1 byte) (1=req, 2= resp), comando, data
 # comandos: 1=date, 2= reverse, 3=download
 
-s = socket.socket()
-host = '127.0.0.1'
-puerto = 2222
+host = '192.168.77.66'
+puerto = 2224
 
 req_header = b'\x01\x01'
 
-s.connect((host, puerto))
-
 
 def main():
+    enviar_fecha_req()
+    enviar_reverse_req()
+    enviar_descargar_req()
     enviar_invalido_req()
 
 
 def enviar_fecha_req():
+    s = socket.socket()
+    s.connect((host, puerto))
     s.send(req_header + b'\x01')
     data = s.recv(1024)
+    s.close()
     print(data)
 
 
 def enviar_reverse_req(data=b"hola"):
+    s = socket.socket()
+    s.connect((host, puerto))
     s.send(req_header + b'\x02' + data)
     data = s.recv(1024)
+    s.close()
     print(data)
 
 
 def enviar_descargar_req(data=b"http://midescarga.com/"):
+    s = socket.socket()
+    s.connect((host, puerto))
     s.send(req_header + b'\x03' + data)
     data = s.recv(1024)
+    s.close()
     print(data)
 
 
 def enviar_invalido_req():
+    s = socket.socket()
+    s.connect((host, puerto))
     s.send(req_header + b'\x08')
     data = s.recv(1024)
+    s.close()
     print(data)
 
 
