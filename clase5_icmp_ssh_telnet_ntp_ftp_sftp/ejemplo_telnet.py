93df4d3a588f412652ea60676bb21e175582bee5
Ivan <ivantm24@gmail.com>
2020-02-19 20:28:00 -0400

    agrega comentario

--- clase5_icmp_ssh_telnet_ntp_ftp_sftp/ejemplo_telnet.py
+++ clase5_icmp_ssh_telnet_ntp_ftp_sftp/ejemplo_telnet.py
@@ -2,6 +2,7 @@ import time
 from telnetlib import Telnet
 
 with Telnet('192.168.56.1', 5004) as tn:
+    # La ip de arriba es la ip y el puerto que GNS3 muestra en el panel superior izquierdo
     print(tn.write(b"show ip\r\n"))
     time.sleep(5)
     print(tn.read_very_eager())
