8ee4416c373dd1b111b14366ad969c17f037f266
Ivan <ivantm24@gmail.com>
2020-03-04 19:33:01 -0400

    ejemplo scrapy fin

--- clase6_scraper/ejemplo_scrapy/ejemplo_scrapy/spiders/arana_citas.py
+++ clase6_scraper/ejemplo_scrapy/ejemplo_scrapy/spiders/arana_citas.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import CitaItem
 
 
 class AranaCita(scrapy.Spider):
@@ -8,4 +9,15 @@ class AranaCita(scrapy.Spider):
     ]
 
     def parse(self, response):
-        pass
+        elemento = CitaItem()
+        all_citas_div = response.css('.quote')
+
+        for cita in all_citas_div:
+            elemento['texto'] = cita.css(".text::text").extract()
+            elemento['autor'] = cita.css(".author::text").extract()
+            yield elemento
+
+        link_siguiente = response.css(".next a::attr(href)").get()
+
+        if link_siguiente is not None:
+            yield response.follow(link_siguiente, callback=self.parse)
